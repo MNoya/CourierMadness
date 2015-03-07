d5b6a8bcd3680c5dcacbe2c305b8d2b41f6d68c6
MNoya <martinnoya@gmail.com>
2015-03-07 04:32:44 -0200

    Workshop Release

---
+++ resource/flash3/PlayerSay.as
@@ -0,0 +1,42 @@
+﻿// This is used to kill the player writing cheats
+package {
+	import flash.display.MovieClip;
+
+	//import some stuff from the valve lib
+	import ValveLib.Globals;
+	import ValveLib.ResizeManager;
+	
+	public class PlayerSay extends MovieClip{
+		
+		//these three variables are required by the engine
+		public var gameAPI:Object;
+		public var globals:Object;
+		public var elementName:String;
+		
+		//constructor, you usually will use onLoaded() instead
+		public function PlayerSay() : void {
+			trace("[PlayerSay] PlayerSay UI Constructed!");
+		}
+		
+		//this function is called when the UI is loaded
+		public function onLoaded() : void {			
+			var oldChatSay:Function = globals.Loader_hud_chat.movieClip.gameAPI.ChatSay;
+			globals.Loader_hud_chat.movieClip.stopMessageMode()
+			globals.Loader_hud_chat.movieClip.gameAPI.ChatSay = function(obj:Object, bool:Boolean){
+				var type:int = globals.Loader_hud_chat.movieClip.m_nLastMessageMode
+				if (bool)
+					type = 4
+				
+				gameAPI.SendServerCommand( "player_say " + type + " " + obj.toString());
+				globals.Loader_hud_chat.movieClip.hud_chat_input.InputBox.text = ""
+				oldChatSay("", bool);
+				//globals.Loader_hud_chat.movieClip.stopMessageMode() //kills the chat but also the hotkeys
+			};
+		}
+		
+		//this handles the resizes - credits to Nullscope
+		public function onResize(re:ResizeManager) : * {
+			
+		}
+	}
+}
---
+++ scripts/vscripts/camera.lua
@@ -0,0 +1,47 @@
+-- Because of how SetCameraTarget works, we have to constantly update the camera to snap it on a unit, by removing and recreating it
+-- While SetCameraTarget is doing its 1~2 second repositioning, it's not possible to move with edge panning
+-- All units will be unselectable so its not possible to lose selection of the hero
+function CameraFollow( event )
+	local initial_pos = Vector(120,-1180,128)
+	local hero = event.caster
+	hero.camera = Entities:FindByName(nil, "camera_guy") -- Initial camera unit on the map, has the initial Y and Z which should never change
+
+	if hero.camera == nil then
+		hero.camera = CreateUnitByName("camera_guy", initial_pos, false, nil, nil, hero:GetTeamNumber())
+	end
+
+	PlayerResource:SetCameraTarget(0, hero.camera)
+	--DebugDrawCircle(hero.camera:GetAbsOrigin(), Vector(255,0,0), 255, 10, true, 2)
+
+
+	-- Camera Updater
+	-- Every second the camera is put in front of the hero pos
+	-- This might interact wrongly with some Move Left/Right combination,
+	-- Needs testing to find the best time interval 
+	Timers:CreateTimer(2.0, function()
+		local newX = hero:GetAbsOrigin().x
+		--print("Updating camera to "..newX)
+		UpdateCamera(hero, newX)
+		return 2.0	
+	end)
+end
+
+-- When a MoveLeft/MoveRight order is executed, the hero.camera will be set to spawn +/-200 X away from the hero's original position
+-- This does't take care of the player using Stop/Hold or change the movement direction, but should help in making it less choppy
+function UpdateCamera( hero, newX )
+	local old_camera = hero.camera:GetAbsOrigin()
+	local pos_camera = Vector(newX , old_camera.y, old_camera.z)
+
+	local new_camera = CreateUnitByName(hero.camera:GetUnitName(), pos_camera, false, nil, nil, hero.camera:GetTeamNumber())
+	hero.camera:RemoveSelf()
+	hero.camera = new_camera
+
+	if hero:IsAlive() then
+		PlayerResource:SetCameraTarget(0, hero.camera)
+		--DebugDrawCircle(hero.camera:GetAbsOrigin(), Vector(255,0,0), 255, 10, true, 1)
+	else
+		PlayerResource:SetCameraTarget(0, hero)
+	end
+end
+
+	
